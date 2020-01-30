a2fbfb894dbc508e562077ab3be253e3b0aa40f7
lobana alarabi <loubanaeng@gmail.com>
2020-01-30 01:46:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ function showBre(json)
 {
     let listBre = document.getElementById('dog-breeds');
     //let sel=document.getElementById('breed-dropdown');
-  
+    listBre.innerHTML="";
     for(let element in json.message)
     {
       if(element[0] == document.getElementById("breed-dropdown").value)
